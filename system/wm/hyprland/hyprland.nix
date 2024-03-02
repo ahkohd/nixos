43097be8fe57ed728bf8bb4f5f90c58d41e941dc
Victor Aremu <me@victorare.mu>
2024-03-02 23:50:12 +0100

    feat: add xdg portal support

--- system/wm/hyprland/hyprland.nix
+++ system/wm/hyprland/hyprland.nix
@@ -6,5 +6,26 @@
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
   };
 
-  environment.systemPackages = with pkgs; [ wl-clipboard waypaper swaybg rofi ];
+  environment.systemPackages = with pkgs; [
+    xdg-desktop-portal-hyprland
+    xdg-utils
+    wl-clipboard
+    waypaper
+    swaybg
+    rofi
+  ];
+
+  environment.sessionVariables = {
+    XDG_SESSION_TYPE = "wayland";
+    XDG_SESSION_DESKTOP = "Hyprland";
+    XDG_CURRENT_DESKTOP = "Hyprland";
+  };
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+    xdgOpenUsePortal = false;
+  };
+
+  xdg.portal.config.common.default = [ "*" ];
 }
