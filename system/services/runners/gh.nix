a10be6bda698f38e6c67cb237e69d21c5ae4b5ac
Victor Aremu <me@victorare.mu>
2024-03-04 16:32:54 +0100

    chore: downscale self hosted runner

--- system/services/runners/gh.nix
+++ system/services/runners/gh.nix
@@ -9,12 +9,5 @@
       tokenFile = config.sops.secrets."tn_token".path;
       url = "https://github.com/t3rn/frontend";
     };
-    tn-runner2 = {
-      enable = true;
-      name = "sasuke";
-      extraLabels = [ "nix" ];
-      tokenFile = config.sops.secrets."tn_token".path;
-      url = "https://github.com/t3rn/frontend";
-    };
   };
 }
