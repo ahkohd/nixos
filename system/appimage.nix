c23918b4388844c2d0ff02774c1b0f1f36ea5730
Victor Aremu <me@victorare.mu>
2024-03-28 13:54:45 +0100

    feat: add app image support

---
+++ system/appimage.nix
@@ -0,0 +1,14 @@
+{ config, pkgs, ... }:
+
+{
+  boot.binfmt.registrations.appimage = {
+    wrapInterpreterInShell = false;
+    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+    recognitionType = "magic";
+    offset = 0;
+    mask = "\\xff\\xff\\xff\\xff\\x00\\x00\\x00\\x00\\xff\\xff\\xff";
+    magicOrExtension = "\\x7fELF....AI\\x02";
+  };
+
+  environment.systemPackages = with pkgs; [ appimage-run ];
+}
