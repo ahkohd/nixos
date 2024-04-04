1f269b4ea70a5953faf6bb384b7fe7cd65425940
Victor Aremu <me@victorare.mu>
2024-04-04 08:22:59 +0100

    feat: switch to using neovim nightly

--- home.nix
+++ home.nix
@@ -35,10 +35,12 @@
 
   # The home.packages option allows you to install Nix packages into your
   # environment.
-  home.packages = [
+  home.packages = with pkgs; [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
-    pkgs.hello
+    hello
+
+    neovim-nightly
 
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
--- overlays.nix
+++ overlays.nix
@@ -1,17 +1,21 @@
-{ config, pkgs, lib, ... }:
+{ config, pkgs, lib, inputs, ... }:
 
-{
-  nixpkgs.overlays = [
-    (self: super: {
-      # https://github.com/NixOS/nixpkgs/pull/292253
-      roon-server = (import (builtins.fetchTarball {
-        url =
-          "https://github.com/NixOS/nixpkgs/archive/aaab0c5c0c074d8f90ebe94827839d11e446eea9.tar.gz";
-        sha256 = "1a440d07vwy310jx77j9jsk2m7hlg2vdycg0hnahz6fv8mx5mvdl";
-      }) {
-        localSystem = "x86_64-linux";
-        config = { allowUnfree = true; };
-      }).roon-server;
-    })
+let
+  roon-server = (self: super: {
+    # https://github.com/NixOS/nixpkgs/pull/292253
+    roon-server = (import (builtins.fetchTarball {
+      url =
+        "https://github.com/NixOS/nixpkgs/archive/aaab0c5c0c074d8f90ebe94827839d11e446eea9.tar.gz";
+      sha256 = "1a440d07vwy310jx77j9jsk2m7hlg2vdycg0hnahz6fv8mx5mvdl";
+    }) {
+      localSystem = "x86_64-linux";
+      config = { allowUnfree = true; };
+    }).roon-server;
+  });
+in {
+  nixpkgs.overlays = with inputs; [
+    roon-server
+
+    neovim-nightly-overlay.overlay
   ];
 }
