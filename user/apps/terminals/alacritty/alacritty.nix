74032c65e4cde1928a9d182f4360c5d26ea612fa
Victor Aremu <me@victorare.mu>
2024-04-04 08:38:06 +0100

    feat: migrate alacritty config
    feat: switch to using oceanic next theme

--- user/apps/terminals/alacritty/alacritty.nix
+++ user/apps/terminals/alacritty/alacritty.nix
@@ -3,45 +3,48 @@
 {
   programs.alacritty.enable = true;
 
-  home.file.".config/alacritty/alacritty.yml".text = ''
-    shell:
-      program: zsh
-    # env:
-    #   TERM: alacritty
-    window:
-      decorations: None
-      dimensions:
-        columns: 140
-        lines: 38
-      opacity: 1
-      padding:
-        x: 10
-        y: 5
-    font:
-      size: 14
-      normal:
-        # family: "BerkeleyMono Nerd Font Plus Font Awesome Plus Octicons Plus Codicons Plus Material Design Icons"
-        family: Bitstream Vera Sans Mono
-        style: Regular
-      bold:
-        # family: "BerkeleyMono Nerd Font Plus Font Awesome Plus Octicons Plus Codicons Plus Material Design Icons"
-        family: Bitstream Vera Sans Mono
-        style: Bold
-      italic:
-        # family: "BerkeleyMono Nerd Font Plus Font Awesome Plus Octicons Plus Codicons Plus Material Design Icons"
-        family: Bitstream Vera Sans Mono
-        style: Italic
-      bold_italic:
-        # family: "BerkeleyMono Nerd Font Plus Font Awesome Plus Octicons Plus Codicons Plus Material Design Icons"
-        family: Bitstream Vera Sans Mono
-        style: Bold Italic
-      offset:
-        x: 0
-        y: 0
-    import:
-      - ~/.config/alacritty/themes/modus_vivendi.yml
+  home.file.".config/alacritty/alacritty.toml".text = ''
+    import = [ "~/.config/alacritty/themes/oceanic_next.toml" ]
+
+    [shell]
+    program = "zsh"
+
+    [window]
+    decorations = "None"
+    opacity = 1
+
+    [window.dimensions]
+    columns = 140
+    lines = 38
+
+    [window.padding]
+    x = 10
+    y = 5
+
+    [font]
+    size = 14
+
+    [font.normal]
+    family = "Bitstream Vera Sans Mono"
+    style = "Regular"
+
+    [font.bold]
+    family = "Bitstream Vera Sans Mono"
+    style = "Bold"
+
+    [font.italic]
+    family = "Bitstream Vera Sans Mono"
+    style = "Italic"
+
+    [font.bold_italic]
+    family = "Bitstream Vera Sans Mono"
+    style = "Bold Italic"
+
+    [font.offset]
+    x = 0
+    y = 0
   '';
 
-  home.file.".config/alacritty/themes/modus_vivendi.yml".source =
-    ./themes/modus_vivendi.yml;
+  home.file.".config/alacritty/themes/oceanic_next.toml".source =
+    ./themes/oceanic_next.toml;
 }
