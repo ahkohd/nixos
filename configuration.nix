4101c239eeac8fe37e2341d5d529cdd573fa9e6a
Victor Aremu <me@victorare.mu>
2024-05-05 22:16:38 +0100

    chore: open port

--- configuration.nix
+++ configuration.nix
@@ -116,7 +116,7 @@
   services.openssh.enable = true;
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 ];
+  networking.firewall.allowedTCPPorts = [ 22 55000 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
