e725f4447ec76159cc957e93cda9f8d381c1c633
Victor Aremu <me@victorare.mu>
2024-05-31 04:40:57 +0100

    refactor: remove zellij bash and zsh integration

--- user/cli/zellij.nix
+++ user/cli/zellij.nix
@@ -3,8 +3,6 @@
 {
   programs.zellij = {
     enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
     settings = {
       theme = "nord";
       simplified_ui = true;
