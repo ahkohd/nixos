5d98e6c5f442bf3af4966b83f7359b199bb96764
Victor Aremu <me@victorare.mu>
2024-06-10 22:36:56 +0100

    chore: use nord theme for btop

--- user/cli/btop.nix
+++ user/cli/btop.nix
@@ -4,8 +4,8 @@
   programs.btop = {
     enable = true;
     settings = {
-      color_theme = "gruvbox_material_dark";
-      theme_background = true;
+      color_theme = "nord";
+      theme_background = false;
     };
   };
 }
