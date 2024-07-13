41cbce20f26d5c4957be0b8100707bfd1e88ee47
Victor Aremu <me@victorare.mu>
2024-07-13 03:58:30 +0100

    feat: configure ghostty

--- system/apps/ghostty.nix
+++ system/apps/ghostty.nix
@@ -1,4 +1,19 @@
 { system, ghostty, ... }:
 
 let gsty = ghostty.packages.${system}.default;
-in { environment.systemPackages = [ gsty ]; }
+
+in {
+  environment.systemPackages = [ gsty ];
+
+  home.file.".config/ghostty/config".text = ''
+    font-family = GeistMono Nerd Font
+
+    font-size = 15
+
+    theme = Nord
+
+    window-padding-x = 20
+
+    window-padding-y = 10
+  '';
+}
