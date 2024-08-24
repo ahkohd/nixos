f891a604bde4dfcff918bce632d2afd6b0761c37
Victor Aremu <me@victorare.mu>
2024-08-25 00:45:04 +0100

    feat: config ghostty

--- user/apps/terminals/ghostty/ghostty.nix
+++ user/apps/terminals/ghostty/ghostty.nix
@@ -3,8 +3,9 @@
 {
 
   home.file.".config/ghostty/config".text = ''
-    font-family = Berkeley Mono
+    # [appearance]
 
+    font-family = Berkeley Mono
     # font-family = GeistMono Nerd Font
 
     font-size = 14
@@ -13,6 +14,50 @@
 
     window-padding-y = 10
 
+    window-width = 150
+
+    window-height = 48 
+
     theme = Solarized Dark - Patched
+
+    # [keybinds]
+
+    keybind = alt+d=new_split:right
+
+    keybind = alt+shift+d=new_split:down
+
+    keybind = alt+t=new_tab
+
+    keybind = alt+shift+h=previous_tab
+
+    keybind = alt+shift+l=next_tab
+
+    keybind = alt+h=goto_split:left
+
+    keybind = alt+l=goto_split:right
+
+    keybind = alt+k=goto_split:top
+
+    keybind = alt+shift+k=goto_split:previous
+
+    keybind = alt+shift+j=goto_split:next
+
+    keybind = alt+j=goto_split:bottom
+
+    keybind = alt+,=reload_config
+
+    keybind = alt+w=close_surface
+
+    keybind = alt+enter=toggle_split_zoom
+
+    keybind = alt+shift+enter=toggle_split_zoom
+
+    # [decorations]
+
+    gtk-titlebar = false
+
+    # [behaviour]
+
+    mouse-hide-while-typing = true
   '';
 }
