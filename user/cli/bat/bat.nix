beb63f3be7035a4774ccae5fcf24fb36ecab5066
Victor Aremu <me@victorare.mu>
2024-10-11 10:35:17 +0100

    feat: add toyonight bat theme

--- user/cli/bat/bat.nix
+++ user/cli/bat/bat.nix
@@ -3,6 +3,9 @@
 {
   programs.bat = {
     enable = true;
-    config = { theme = "ansi"; };
+    config = { theme = "tokyonight-storm"; };
   };
+
+  home.file.".config/bat/themes/tokyonight-storm.tmTheme".text =
+    builtins.readFile ./themes/tokyonight-storm.tmTheme;
 }
