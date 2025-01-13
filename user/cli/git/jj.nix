a27445b9fa8f7b47735202c90dcde6e9990664d4
Victor Aremu <me@victorare.mu>
2025-01-13 04:15:03 +0100

    configure delta for jj diff ui

--- user/cli/git/jj.nix
+++ user/cli/git/jj.nix
@@ -12,7 +12,11 @@
         backend = "gpg";
         key = "1F76A9056C921C8A004F63E28D43F8A176DDC6BD";
       };
-      ui = { diff-editor = [ "nvim" "-c" "DiffEditor $left $right $output" ]; };
+      ui = {
+        pager = "delta";
+        diff = { format = "git"; };
+        diff-editor = [ "nvim" "-c" "DiffEditor $left $right $output" ];
+      };
       snapshot = { max-new-file-size = 6110460; };
     };
   };
