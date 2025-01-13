3ba2cb22ffaae50b349632a52e1873245a41453f
Victor Aremu <me@victorare.mu>
2025-01-13 04:00:45 +0100

    use delta for jj diff

--- user/cli/git/jj.nix
+++ user/cli/git/jj.nix
@@ -13,7 +13,11 @@
         backend = "gpg";
         key = "1F76A9056C921C8A004F63E28D43F8A176DDC6BD";
       };
-      ui = { diff-editor = [ "nvim" "-c" "DiffEditor $left $right $output" ]; };
+      ui = {
+        pager = "delta";
+        diff = { format = "git"; };
+        diff-editor = [ "nvim" "-c" "DiffEditor $left $right $output" ];
+      };
     };
   };
 }
